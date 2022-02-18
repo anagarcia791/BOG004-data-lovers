c6946c6e4bb6c5031a5abf2314efd3b9d4e6fee8
anagarcia791 <amgt2697@gmail.com>
2022-02-18 12:42:02 -0500

    se corrige valor esperado

--- test/data.spec.js
+++ test/data.spec.js
@@ -23,15 +23,23 @@ const testData = {
 
 describe('example', () => {
   it('returns `example`', () => {
-    const result = filterHandler('grass', true, testData, []);
+    const result = filterHandler('grass', true, testData.pokemon);
     expect(result).toHaveLength(1);
   });
   it('returns `example` 2', () => {
-    const result = filterHandler('poison', true, testData, []);
-    expect(result).toHaveLength(2);
+    const result = filterHandler('poison', true, testData.pokemon);
+    expect(result).toHaveLength(3);
   });
   it('returns `example` 3', () => {
-    const result = filterHandler('ice', true, testData, []);
+    const result = filterHandler('grass', false, testData.pokemon);
+    expect(result).toHaveLength(2);
+  });
+  it('returns `example` 4', () => {
+    const result = filterHandler('ice', true, testData.pokemon);
+    expect(result).toHaveLength(3);
+  });
+  it('returns `example` 5', () => {
+    const result = filterHandler('poison', false, testData.pokemon);
     expect(result).toHaveLength(1);
   });
 });
